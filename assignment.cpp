03fbbae45985e1eebf3e40a3e2733b9b5cbc3547
Zeaforx <ajibcaleb@gmail.com>
2024-12-01 01:44:26 +1300

    single file

--- assignment.cpp
+++ assignment.cpp
@@ -256,17 +256,17 @@ int main()
     };
 
     QuestionClass obj;
-    // obj.question2A();
-    // obj.question2B();
-    // obj.question3A();
+    obj.question2A();
+    obj.question2B();
+    obj.question3A();
     obj.question3B();
-    // obj.question4A();
-    // obj.question4B();
-    // obj.question5A();
-    // obj.question5B();
-    // obj.question6A();
-    // obj.question6B();
-    // obj.question7A();
-    // obj.question7B();
+    obj.question4A();
+    obj.question4B();
+    obj.question5A();
+    obj.question5B();
+    obj.question6A();
+    obj.question6B();
+    obj.question7A();
+    obj.question7B();
     return 0;
 }
