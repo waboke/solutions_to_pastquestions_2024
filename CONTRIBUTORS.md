3db2d2c3e64424562cd61d5540af7b865e239c4d
Zeaforx <ajibcaleb@gmail.com>
2024-11-30 22:51:39 +1300

    first commit

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -1,7 +1,12 @@
 # CONTRIBUTORS.
+
 ## Contributors should add thier names, github username and matric number here.
 
 <ol>
 <li>William Rupert Waboke
+
+Name: Ajibade Caleb
+Github username: Zeaforx
+Matric no: U22CS1020
 
 </ol>
---
+++ assignment.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "questionClass.h"
+using namespace std;
+
+int main()
+{
+    QuestionClass obj;
+    obj.question2A();
+    obj.question2B();
+    obj.question3A();
+    obj.question3B();
+    obj.question4A();
+    obj.question4B();
+    obj.question5A();
+    obj.question5B();
+    obj.question6A();
+    obj.question6B();
+    obj.question7A();
+    return 0;
+}
